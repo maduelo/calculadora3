a39f867e189d4fdfa27418d0034ed5b41db955ef
Sergi Maduell <maduelo@hotmail.com>
2014-11-06 20:32:13 +0100

    calculadora cientifica

--- calculadora3/src/calculadora3/calculadora3.java
+++ calculadora3/src/calculadora3/calculadora3.java
@@ -3,7 +3,7 @@ package calculadora3;
 public class calculadora3 {
 
 	public static void main(String[] args) {
-		System.out.println("Sóc una calculadora simple");
+		System.out.println("Sóc una calculadora cientifica");
 
 	}
 
